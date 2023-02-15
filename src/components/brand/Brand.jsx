02fffedda72add973f7822c97c5dec8d3bf67b35
ajhar10 <ajharrimon90@gmail.com>
2023-02-15 16:15:43 +0600

    header and navbar section done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, atlassian, dropbox, shopify, slack } from "./import";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt__brand section_padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import Features from "../features/Features";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt__wgpt section_margin" id="wgpt">
+      <div className="gpt__wgpt-feature">
+        <Features />
+      </div>
+      <div className="gpt__wgpt-heading">
+        <h1 className="gradient_text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt__wgpt-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
